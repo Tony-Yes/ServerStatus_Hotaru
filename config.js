ec90225c5e34bb2ce2aed7552d004eddf15b7909
TL067 <491364@gmail.com>
2022-12-15 21:15:44 +0800

    test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.__PRE_CONFIG__ = {
-  header: '',
-  subHeader: '',
+  header: 'Server Status',
+  subHeader: 'Servers\' Probes Set up with ServerStatus',
   interval: 1.5,
-  footer: ''
+  footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
 };
